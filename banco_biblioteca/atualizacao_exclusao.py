660e491c033a217d3d84ba805fcc97f29ec0ac8f
Roberta Oliveira <102266840+RobertaOliveira07@users.noreply.github.com>
2024-08-26 21:09:44 -0300

    incluindo metodo de exclusão e atualização

--- banco_biblioteca/atualizacao_exclusao.py
+++ banco_biblioteca/atualizacao_exclusao.py
@@ -22,3 +22,27 @@ def remover_livro(conn, livro_id):
     query = "DELETE FROM Livros WHERE id = ?"
     conn.execute(query, (livro_id))
     conn.commit()
+    
+#Atualizar informações do usuario
+def atualizar_usuario(conn, usuario_id, novo_nome, novo_email):
+    query = "UPDATE Usuario SET nome = ?, email = ? WHERE id_usuario = ?"
+    conn.execute(query, (novo_nome, novo_email, usuario_id))
+    conn.commit()
+    
+#Atualizar informações do autor
+def atualizar_autor(conn, autor_id, novo_nome=None, nova_nacionalidade=None):
+    if novo_nome is not None:
+        query = "UPDATE Autor SET nome = ? WHERE id_autor = ?"
+        conn.execute(query, (novo_nome, autor_id))
+    if nova_nacionalidade is not None:
+        query = "UPDATE Autor SET nacionalidade = ? WHERE id_autor = ?"
+        conn.execute(query, (nova_nacionalidade, autor_id))
+    conn.commit()
+    
+#Remover emprestimo
+def remover_emprestimo(conn, emprestimo_id):
+    query = "DELETE FROM Emprestimo WHERE id_emprestimo = ?"
+    conn.execute(query, (emprestimo_id,))
+    conn.commit()
+
+
